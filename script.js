f025ac61ebcd311c910556ecaf7d5e6fd33987af
suryaprdv <169064200+suryaprdv@users.noreply.github.com>
2025-05-18 13:51:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ mobileMenu.addEventListener('click', () => {
   navList.classList.toggle('active');
 });
 
-<script>
+
   const toggleBtn = document.getElementById('menu-toggle');
   const navList = document.getElementById('nav-list');
 
   toggleBtn.addEventListener('click', () => {
     navList.classList.toggle('active');
   });
-</script>
+
